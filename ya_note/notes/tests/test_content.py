7aa7436adb3333f1dfb1c19c5c5729feab81e5ea
Данила Агапов <7904514482@mail.ru>
2025-01-27 13:23:56 +0300

    Add_test_non_author

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -7,22 +7,25 @@ from .base_test import (
 from notes.forms import NoteForm
 
 
-class TestPages(TestBase):
+class TestPages(TestBase): 
+
+    def test_note_not_displayed_for_non_author(self):
+        response = self.client_reader.get(URL_NOTES_LIST)
+        displayed_notes = response.context['object_list']
+        self.assertNotIn(self.note1, displayed_notes)
+
     def test_displaying_note_in_list(self):
         response = self.client_author.get(URL_NOTES_LIST)
-        displayed_notes = list(response.context['object_list'])
-        self.assertIn(self.note, displayed_notes)
-        for field in ['title', 'text', 'slug', 'author']:
-            self.assertEqual(getattr(self.note, field), getattr(
-                displayed_notes[displayed_notes.index(self.note)],
-                field))
+        displayed_notes = response.context['object_list']
+        self.assertIn(self.note1, displayed_notes)
+        displayed_note = displayed_notes.get(pk=self.note1.pk)
+        self.assertEqual(self.note1.title, displayed_note.title)
+        self.assertEqual(self.note1.text, displayed_note.text)
+        self.assertEqual(self.note1.slug, displayed_note.slug)
+        self.assertEqual(self.note1.author, displayed_note.author)
 
     def test_transfer_form_for_adding_editing_notes(self):
-        for url in (
-            URL_NOTES_ADD,
-            URL_NOTES_EDIT,
-        ):
+        for url in (URL_NOTES_ADD, URL_NOTES_EDIT):
             with self.subTest(url=url):
-                assert isinstance(
-                    self.client_author.get(url).context.get('form'), NoteForm
-                )
+                form = self.client_author.get(url).context.get('form')
+                self.assertIsInstance(form, NoteForm)
